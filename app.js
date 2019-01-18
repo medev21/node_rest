971a921fe86ca664be8ce98f90fb7d95f67c8247
medev21 <benmarteen07@gmail.com>
2019-01-18 17:13:28 -0500

    install body parser package

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
@@ -11,6 +12,10 @@ const orderRoutes = require('./api/routes/orders');
 //     });
 // });
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
