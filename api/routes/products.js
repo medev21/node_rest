3b46abe94f679a92b4c594b4720c43c613d61ab5
medev21 <benmarteen07@gmail.com>
2019-01-22 19:16:33 -0500

    import mongoose in products

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
 router.get('/', (request, response,next) => {
     response.status(200).json({
