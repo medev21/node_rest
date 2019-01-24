09e51b73bcfeebba1f5b40d717eae3491a082630
medev21 <benmarteen07@gmail.com>
2019-01-24 15:34:07 -0500

    save entry for order POST request

--- api/models/orders.js
+++ api/models/orders.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const orderSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    product: {type: mongoose.Schema.Types.ObjectId, ref: 'Product'},//connect reference to Product model
+    product: {type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true},//connect reference to Product model
     quantity:{type: Number, default: 1}
 })
 
--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Order = require('../models/orders');
 
 router.get('/', (req, res) => {
     res.status(200).json({
@@ -8,13 +10,21 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res, next) => {
-    const order = {
-        productId: req.body.productId,
-        quantity: req.body.quantity
-    }
-    res.status(201).json({
-        message: 'order was created',
-        order: order
+    const order = new Order({
+        _id: mongoose.Types.ObjectId(),
+        quantity: req.body.quantity,
+        product: req.body.productId
+    });
+    order.save()
+    .then(result => {
+        console.log(result);
+        res.status(201).json(result)
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            error: err
+        })
     });
 })
 
