b78c8016b854495479b68fd4af3be37b1cc67db0
medev21 <benmarteen07@gmail.com>
2019-02-05 19:17:26 -0500

    add user delete route

--- api/routes/user.js
+++ api/routes/user.js
@@ -44,31 +44,19 @@ router.post('/signup',(req,res,next) => {
         }
     });
 
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if (err){
-            return res.status(500).json({
-                error: err
-            });
-        }else{
-            const user = new User({
-                _id: new mongoose.Types.ObjectId,
-                email: req.body.email,
-                password: hash
-            });
-            user.save()
-            .then(result => {
-                console.log(result);
-                res.status(201).json({
-                    message: 'User created'
-                });
+    router.delete('/:userId', (req,res,next) => {
+        User.deleteOne({_id: req.params.userId})
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: 'User deleted'
             })
-            .catch(err => {
-                console.log(err);
-                res.status(500).json({
-                    error: err
-                });
-            });
-        }
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        });
     });
 });
 
